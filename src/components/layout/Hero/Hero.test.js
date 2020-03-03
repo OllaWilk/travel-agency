5dace77112de83659b08c3202d300b208667de90
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-03-03 15:15:24 +0100

    19.2 test Hero

--- src/components/layout/Hero/Hero.test.js
+++ src/components/layout/Hero/Hero.test.js
@@ -6,17 +6,34 @@ describe('Component Hero', () => {  //describe zgrupuje testy. W pierwszym argum
   it('should render without crashing', () => { //it definiuje pojedynczy test. Opis testu jest w 1 argumencie.
     const component = shallow(<Hero titleText='Lorem ipsum' />); //w stałej component zapisany jest funkcji shallow
     expect(component).toBeTruthy(); //expect sprawdza czy wyrenderowany oiekt jest prawdziwy
+    console.log(component.debug());
   });
 
+  /*TESTOWANIE BRZEGOWE - jak zachowa się dokument kiedy nie podamy propsa*/
 
-  describe('Component Hero', () => {
-    it('should render without crashing', () => {
-      const component = shallow(<Hero titleText='Lorem ipsum' />);
-      expect(component).toBeTruthy();
-    });
+  it('should throw error without required props', () => {
+    expect(() => shallow(<Hero />)).toThrow();
+  });
 
-    it('should throw error without required props', () => {
-      expect(() => shallow(<Hero />)).toThrow();
-    });
+  /*ELEMENTY KOMPONTENTU- */
+
+  it('should render correct title and image', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const expectedImage = 'image.jpg';
+    const component = shallow(<Hero titleText={expectedTitle} imageSrc={expectedImage} />);
+
+    const renderedTitle = component.find('.title').text(); //.find i .text to pakiety dostarczone z Enzyme.
+    expect(renderedTitle).toEqual(expectedTitle);
+    expect(component.find('.image').prop('src')).toEqual(expectedImage);
+  });
+
+  /*SPRAWDZA CZY WYRENDEROWANY WRAPER KOMPONENTU OTRZYMUJE POPRAWNE KLASY W ZALEŻNOŚCI OD PROPSA VARIANT*/
+
+  it('renders correct classNames', () => {
+    const mockVariants = 'small dummy';
+    const component = shallow(<Hero titleText='Lorem' imageSrc='image.jpg' variant={mockVariants} />);
+    expect(component.hasClass('component')).toBe(true);
+    expect(component.hasClass('small')).toBe(true);
+    expect(component.hasClass('dummy')).toBe(true);
   });
 });
