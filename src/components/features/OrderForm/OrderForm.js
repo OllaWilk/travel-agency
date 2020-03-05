3e589235330442645d7fbdce3a794f8a252ff948
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-03-05 21:21:32 +0100

    19.4 add functionality to send to API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,6 +4,38 @@ import OrderSummary from '../OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import settings from '../../../data/settings';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = ({options, tripCost, setOrderOption}) => {
 
@@ -18,6 +50,7 @@ const OrderForm = ({options, tripCost, setOrderOption}) => {
       )}
       <Col xs={12}>
         <OrderSummary cost={tripCost} options={options} />
+        <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
       </Col>
     </Row>
   );
